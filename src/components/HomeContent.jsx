4190347c36fb52ffb3cac9fcabc5c525c7bf32eb
sacostapliego <steven.acosta867@gmail.com>
2025-09-19 23:22:47 -0400

    info box changes

--- src/components/HomeContent.jsx
+++ src/components/HomeContent.jsx
@@ -22,7 +22,20 @@ const HomeContent = () => {
             </div>
           </div>
         </div>
-        
+        <div className="info-box">
+          <div className="box-content">
+            <div className="logo-section">
+              <div className="logo-placeholder">
+                <img src="https://i.namu.wiki/i/8x1mjJ2UvYbdU_0NPGZtRsNDs90SZu668ij9ySjdmr0G5wXbNI1BzO4vVgfEPEPgMGgANc93UJWeCEjD_AdTJg.png" alt="Logo" />
+              </div>
+            </div>
+            <div className="text-section">
+              <div className="text-line">Georgia State University</div>
+              <div className="text-line">Second line of text</div>
+              <div className="text-line">Third line of text</div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
