502e443ac5a7524c6434add88afb14c916ec7fa3
Steven <steven.acosta867@gmail.com>
2025-09-20 12:10:46 -0400

    info box changes

--- src/components/HomeContent.jsx
+++ src/components/HomeContent.jsx
@@ -5,37 +5,30 @@ const HomeContent = () => {
   return (
     <div className="home-content">
       <h1 className="main-heading">STEVEN<br />ACOSTA-PLIEGO</h1>
-      <p className="sub-heading">Self-taught Developer</p>
+      <p className="sub-heading">
+        Hi, my name is Steven. I am a self-taught developer, 
+        and currently pursuing a Bachelor's degree in Computer Science at Georgia State University.
+      </p>
 
       <div className="info-boxes-container">
-        <div className="info-box">
+        {/* Box 1 */}
+        <div className="info-box one">
           <div className="box-content">
             <div className="logo-section">
               <div className="logo-placeholder">
-                <img src="https://i.namu.wiki/i/8x1mjJ2UvYbdU_0NPGZtRsNDs90SZu668ij9ySjdmr0G5wXbNI1BzO4vVgfEPEPgMGgANc93UJWeCEjD_AdTJg.png" alt="Logo" />
+                <img src="/gsu2.png" alt="Logo" />
               </div>
             </div>
             <div className="text-section">
               <div className="text-line">Georgia State University</div>
               <div className="text-line">Bachelor of Science in Computer Science</div>
-              <div className="text-line">GPA: 3.78/4.0</div>
+              <div className="text-line">GPA: 3.78/4.0; Honors</div>
             </div>
           </div>
         </div>
-        <div className="info-box">
-          <div className="box-content">
-            <div className="logo-section">
-              <div className="logo-placeholder">
-                <img src="https://i.namu.wiki/i/8x1mjJ2UvYbdU_0NPGZtRsNDs90SZu668ij9ySjdmr0G5wXbNI1BzO4vVgfEPEPgMGgANc93UJWeCEjD_AdTJg.png" alt="Logo" />
-              </div>
-            </div>
-            <div className="text-section">
-              <div className="text-line">Georgia State University</div>
-              <div className="text-line">Second line of text</div>
-              <div className="text-line">Third line of text</div>
-            </div>
-          </div>
-        </div>
+
+        {/* Box 2 */}
+
       </div>
     </div>
   );
