fda5410542f20f3683ed42208641177d09ffb301
Steven <steven.acosta867@gmail.com>
2025-09-24 23:38:31 -0400

    icons added

--- src/components/home-page/components/SkillsData.jsx
+++ src/components/home-page/components/SkillsData.jsx
@@ -4,19 +4,32 @@
 // Databases: PostgreSQL, MongoDB
 // Tools & Platforms: Git, AWS, Supabase
 
+import pyIcon from '../../assets/skills/pythonicon.svg';
+import jsIcon from '../../assets/skills/javascripticon.svg';
+import tsIcon from '../../assets/skills/typescripticon.svg';
+import javaIcon from '../../assets/skills/javaicon.svg';
+import reactIcon from '../../assets/skills/reacticon.svg';
+import nodeIcon from '../../assets/skills/nodeicon.svg';
+import chakraIcon from '../../assets/skills/chakraicon.svg';
+import postgresIcon from '../../assets/skills/postgresqlicon.svg';
+import mongoIcon from '../../assets/skills/mongoicon.svg';
+import gitIcon from '../../assets/skills/giticon.svg';
+import awsIcon from '../../assets/skills/awsicon.svg';
+import supabaseIcon from '../../assets/skills/supabaseicon.svg';
+
 const skillsData = [
-  { imageSrc: '/images/skills/python.png', title: 'Python', subtitle: 'Backend/AI' },
-  { imageSrc: '/images/skills/python.png', title: 'JavaScript', subtitle: 'Frontend Web' },
-  { imageSrc: '/images/skills/python.png', title: 'TypeScript', subtitle: 'Frontend Web/Mobile' },
-  { imageSrc: '/images/skills/python.png', title: 'Java', subtitle: 'Backend' },
-  { imageSrc: '/images/skills/python.png', title: 'React', subtitle: 'Framework' },
-  { imageSrc: '/images/skills/python.png', title: 'Node.js', subtitle: 'Full-Stack' },
-  { imageSrc: '/images/skills/python.png', title: 'Chakra UI', subtitle: 'Styling' },
-  { imageSrc: '/images/skills/python.png', title: 'PostgreSQL', subtitle: 'Database' },
-  { imageSrc: '/images/skills/python.png', title: 'MongoDB', subtitle: 'Database' },
-  { imageSrc: '/images/skills/python.png', title: 'Git', subtitle: 'Version Control' },
-  { imageSrc: '/images/skills/python.png', title: 'AWS', subtitle: 'Cloud' },
-  { imageSrc: '/images/skills/python.png', title: 'Supabase', subtitle: 'Database' },
+  { imageSrc: pyIcon, title: 'Python', subtitle: 'Backend/AI' },
+  { imageSrc: jsIcon, title: 'JavaScript', subtitle: 'Frontend Web' },
+  { imageSrc: tsIcon, title: 'TypeScript', subtitle: 'Frontend Web/Mobile' },
+  { imageSrc: javaIcon, title: 'Java', subtitle: 'Backend' },
+  { imageSrc: reactIcon, title: 'React', subtitle: 'Frontend' },
+  { imageSrc: nodeIcon, title: 'Node.js', subtitle: 'Packages' },
+  { imageSrc: chakraIcon, title: 'Chakra UI', subtitle: 'Styling' },
+  { imageSrc: postgresIcon, title: 'PostgreSQL', subtitle: 'Local Database' },
+  { imageSrc: mongoIcon, title: 'MongoDB', subtitle: 'Database' },
+  { imageSrc: gitIcon, title: 'Git', subtitle: 'Version Control' },
+  { imageSrc: awsIcon, title: 'AWS', subtitle: 'Cloud/Image Hosting' },
+  { imageSrc: supabaseIcon, title: 'Supabase', subtitle: 'Database' },
 ];
 
 export default skillsData;
