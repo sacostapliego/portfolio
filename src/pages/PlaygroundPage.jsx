a1d991efcb60dae52f9267c8254be0c775cd4198
sacostapliego <steven.acosta867@gmail.com>
2025-09-26 15:33:36 -0400

    playground title change

--- src/pages/PlaygroundPage.jsx
+++ src/pages/PlaygroundPage.jsx
@@ -66,7 +66,7 @@ function PlaygroundPage() {
               mb={{ base: '1.5rem', md: '2rem' }}
               textAlign={'left'}
             >
-              <Text color="rgba(251,247,245)" display="block" mb={'0.5rem'} fontWeight="900">EXPERIMENTS</Text>
+              <Text color="rgba(251,247,245)" display="block" mb={'0.5rem'} fontWeight="900">PLAYGROUND</Text>
               <Text w={{ base: '100%', md: '50%' }} fontSize={{ base: '1rem', md: '2rem', lg: '1.5rem' }} color="rgba(251,247,245)" display="block" mb={'0.5rem'} fontWeight="400">
                 This is just a playground for me to experiment with ideas and technologies.
               </Text>
