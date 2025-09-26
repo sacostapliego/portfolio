b0c9ba56e6011d5d55b0c2893b80ba539c91e397
sacostapliego <steven.acosta867@gmail.com>
2025-09-26 15:30:26 -0400

    mobile change

--- src/pages/PlaygroundPage.jsx
+++ src/pages/PlaygroundPage.jsx
@@ -67,7 +67,7 @@ function PlaygroundPage() {
               textAlign={'left'}
             >
               <Text color="rgba(251,247,245)" display="block" mb={'0.5rem'} fontWeight="900">EXPERIMENTS</Text>
-              <Text w={'50%'} fontSize={{ base: '1rem', md: '2rem', lg: '1.5rem' }} color="rgba(251,247,245)" display="block" mb={'0.5rem'} fontWeight="400">
+              <Text w={{ base: '100%', md: '50%' }} fontSize={{ base: '1rem', md: '2rem', lg: '1.5rem' }} color="rgba(251,247,245)" display="block" mb={'0.5rem'} fontWeight="400">
                 This is just a playground for me to experiment with ideas and technologies.
               </Text>
             </Heading>
