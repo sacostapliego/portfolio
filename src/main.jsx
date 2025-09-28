796b8ecd1e2e74ef274ad0fdbae3715a761660dd
sacostapliego <steven.acosta867@gmail.com>
2025-09-27 23:49:40 -0400

    vercel test

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,13 @@ import { createRoot } from 'react-dom/client'
 import { Provider } from './components/ui/provider.jsx'
 import './index.css'
 import App from './App.jsx'
+import { Analytics } from '@vercel/analytics/react'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider>
       <App />
+      <Analytics />
     </Provider>
   </StrictMode>
 )
