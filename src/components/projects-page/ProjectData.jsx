a03a4a0778590c82078ae8f263a310d54c06ae8d
Steven <steven.acosta867@gmail.com>
2025-12-04 23:10:33 -0500

    Rightside of projects page updated with new key features section Added star icon next to key features heading

--- src/components/projects-page/ProjectData.jsx
+++ src/components/projects-page/ProjectData.jsx
@@ -24,7 +24,6 @@ const projectData = [
     description: 'Full stack web and mobile application to manage and share wishlists, built with React, React Native, Expo, S3, Supabase Auth, and FastAPI.',
     link: 'https://cardinal-wishlist.onrender.com/',
     warning: 'This website is hosted on a free tier and may take up to 2-3 minutes to load.',
-    overview: '',
     github: 'https://github.com/sacostapliego/wishlist'
   },
   {
@@ -47,19 +46,25 @@ const projectData = [
   {
     title: 'BlueJay Computer Catalog',
     images: [blujayPage1, blujayPage2, blujayPage3],
+    keyfeatures: 
+    [
+      'Admins can create, update, and manage custom PC builds by selecting from a database of PC parts, with the ability to set custom prices and view detailed component specifications for the general audience to view.',
+      'Comprehensive security implementation using Spring Security with stateless JWT token-based authentication, supporting role-based authorization (Admin/User roles)',
+      'User profile management with secure image uploads to AWS S3, allowing users to personalize their accounts with profile pictures.',
+    ],
     tags: ['React', 'Chakra UI', 'Vite', 'JavaScript', 'Java', 'Spring Boot', 'PostgreSQL', 'AWS', 'Docker', 'Render'],
     description: 'A full-stack PC parts management system that allows users to browse PC components,  custom builds, and user profiles. Built with React/Vite frontend and Spring Boot backend, the application provides secure user authentication via JWT tokens and stores user profile imagess in AWS S3. The system features role-based access control (Admin/User) and uses PostgreSQL for data persistence with Flyway for database migrations.',
     preview: 'Full stack web application for browsing and managing computer parts, built with React, Spring Boot, and PostgreSQL.',
     link: 'https://blujay-pc-builds.onrender.com/dashboard/parts',
+    github: 'https://github.com/sacostapliego/pc-parts-springboot',
     warning: 'This website is hosted on a free tier and may take up to 3-4 minutes to load.',
-    github: 'https://github.com/sacostapliego/pc-parts-springboot'
   },
   {
     title: 'AI Try-On Closet',
     images: [aicloset],
     tags: ['React', 'Tailwind CSS', 'Vite', 'TypeScript', 'FastAPI', 'PostgreSQL', 'pgAdmin', 'Gemini AI', 'Hugging Face'],
     description: 'A full-stack AI-powered closet that allows users to upload photos and generate photorealistic images of themselves wearing selected clothing items. Built with a React + Tailwind CSS frontend and a FastAPI backend, it integrates Google Gemini AI for image generation. Users can manage a personal wardrobe and view their generated outfit history in an interactive, game-inspired interface.',
-    preview: 'A full-stack AI-powered closet that allows users to upload photos and generate photorealistic images of themselves wearing selected clothing items. Built with a React + Tailwind CSS frontend and a FastAPI backend, it integrates Google Gemini AI for image generation. Users can manage a personal wardrobe and view their generated outfit history in an interactive, game-inspired interface.',
+    preview: 'A AI Virtual closet allows the user to upload their own image of themseleves or anyone along with any shirts, jackets, and pants. The user can than combine either a shirt/jacket and/or pants and generate it to see what it would look like.',
     warning: 'This is only able to work locally',
     github: 'https://github.com/sacostapliego/penguin-outfits',
   },
--- src/components/projects-page/individual-project-pages/RightSide.jsx
+++ src/components/projects-page/individual-project-pages/RightSide.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import { useParams } from 'react-router-dom';
-import { Box, Image, IconButton, VStack } from '@chakra-ui/react';
-import { FaChevronLeft, FaChevronRight } from 'react-icons/fa';
+import { Box, Image, IconButton, VStack, Heading, Text, List, ListItem } from '@chakra-ui/react';
+import { FaChevronLeft, FaChevronRight, FaRegStar } from 'react-icons/fa';
 import projectData from '../ProjectData';
 
 function RightSideProjectsPage() {
@@ -26,7 +26,7 @@ function RightSideProjectsPage() {
   };
 
   return (
-    <VStack spacing={4} align="stretch" w="100%">
+    <VStack gap={{base:'0.5rem'}} align="stretch" w="100%">
       <Box 
         position="relative" 
         mb="1.5rem"
@@ -77,6 +77,38 @@ function RightSideProjectsPage() {
           </>
         )}
       </Box>
+
+      {project.keyfeatures && project.keyfeatures.length > 0 && (
+        <Box >
+          <Heading
+            as="h2"
+            fontSize={{ base: "1.2rem", md: "1.2rem", '2xl': "1.5rem" }}
+            mb="1rem"
+            color="rgba(251,247,245)"
+            fontWeight="700"
+            display="flex"
+            alignItems="center"
+            gap={'0.5rem'}
+          >
+            <FaRegStar color="white" />
+            Key Features
+          </Heading>
+          
+          <List.Root gap={'1rem'} pl={'2rem'}>
+            {project.keyfeatures.map((feature, index) => (
+              <List.Item
+                key={index}
+                fontSize="1rem"
+                color="gray.300"
+                lineHeight="1.6"
+              >
+                {feature}
+              </List.Item>
+            ))}
+          </List.Root>
+        </Box>
+      )}
+
     </VStack>
   );
 }
